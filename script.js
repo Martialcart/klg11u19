4a60672936b63320b9c0d08557ab44f09e759963
Martialcart <janolav91@hotmail.com>
2024-12-05 12:00:02 +0100

    delete and finished buttons working

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ console.log(`inputField: ${inputField}
             todoList: ${todoList}`);
 
 addButton.addEventListener('click', function () {
+    console.log("trykket på addButton");
     
     const todoItem = document.createElement('li');
     todoItem.textContent = inputField.value;
@@ -14,15 +15,28 @@ addButton.addEventListener('click', function () {
 
     inputField.value = "";
 
-    const deleteItem = document.createElement("button");
-    deleteItem.textContent ="Slett";
-    todoItem.appendChild(deleteItem);
-
     const doneItem = document.createElement("button");
     doneItem.textContent ="ferdig";
     todoItem.appendChild(doneItem);
+    doneItem.addEventListener('click', function () {
+        console.log("trykket på ferdig");
+        if (todoItem.classList.contains("done")) {
+            todoItem.classList.remove("done");
+        } else {
+            todoItem.classList.add("done");
+        }
+    });
 
-   
+    const deleteItem = document.createElement("button");
+    deleteItem.textContent ="Slett";
+    todoItem.appendChild(deleteItem);
+    deleteItem.addEventListener('click', function () {
+        console.log("trykket på slett");
+        if(todoItem.classList.contains("done")) {
+            todoItem.remove();
+        } else {
+            alert("Gjøremål må være ferdig før et kan slettes");
+        }
+    });  
 
-    console.log(`trykket på addButton`);
 });
