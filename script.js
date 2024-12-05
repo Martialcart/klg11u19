d90e5c31b27f469710d6ff6e78505ccf7dc42d25
Martialcart <janolav91@hotmail.com>
2024-12-05 10:55:47 +0100

    Adding a todo item with button works

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ console.log(`inputField: ${inputField}
             addButton: ${addButton}
             todoList: ${todoList}`);
 
+addButton.addEventListener('click', function () {
+    
+    const todoItem = document.createElement('li');
+    todoItem.textContent = inputField.value;
+    todoList.appendChild(todoItem);
+
+    inputField.value = "";
+
+    console.log(`trykket på addButton`);
+});
