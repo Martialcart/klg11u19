9158c2ff5f00daa637fe2dbcf0afff77e1938c6b
Martialcart <janolav91@hotmail.com>
2024-12-05 10:20:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputField = document.querySelector(#inputField);
+const addButton = document.querySelector(#addButton);
+const todoList = document.querySelector(#todoList);
+
+console.log(`inputField: ${inputField}
+            addButton: ${addButton}
+            todoList: ${todoList}`);
+
+            
