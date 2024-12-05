41929b3576e23541e0e5bccd03be381acc6a92d0
Martialcart <janolav91@hotmail.com>
2024-12-05 10:21:58 +0100

    typo

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const inputField = document.querySelector(#inputField);
-const addButton = document.querySelector(#addButton);
-const todoList = document.querySelector(#todoList);
+const inputField = document.querySelector('#inputField');
+const addButton = document.querySelector('#addButton');
+const todoList = document.querySelector('#todoList');
 
 console.log(`inputField: ${inputField}
             addButton: ${addButton}
             todoList: ${todoList}`);
 
-            
